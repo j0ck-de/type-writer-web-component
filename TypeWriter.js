79ec3e93064a88de7f847ba76c60d66eb5f5c5b4
j0ck-de <josef.molz1985@gmail.com>
2023-03-09 13:04:28 +0100

    edited some stuff

--- TypeWriter.js
+++ TypeWriter.js
@@ -3,7 +3,7 @@ template.innerHTML = `
 
   <style>
     .txt {
-      color: var(--color, #666);
+      color: var(--color, #000);
       background: var(--background, transparent);
       font-size: var(--font-size, 1.5rem);
       font-family: var(--font-family, "Courier New", Courier, monospace);
@@ -13,7 +13,7 @@ template.innerHTML = `
     .type-writer {
       display: inline-block;
       height: 100%;
-      border-right: 0.15em solid #666;
+      border-right: var(--indicator-width, 2.5px) solid var(--color, #000);
       animation: blink-caret var(--indicator-speed, 0.75s) step-end infinite;
     
     }
@@ -29,7 +29,7 @@ template.innerHTML = `
         border-color: transparent;
       }
       50% {
-        border-color: #666;
+        border-color: var(--color, #000);
       }
     }
          
@@ -44,7 +44,8 @@ class TypeWriter extends HTMLElement {
     this.shadow.appendChild(template.content.cloneNode(true));
     this.txtContainer = this.shadow.querySelector(".type-writer");
     this.words = JSON.parse(
-      this.getAttribute("data-words") ?? '["First", "Second", "Third"]'
+      this.getAttribute("data-words") ??
+        '["Text 1", "Text 2", "Text 3", "Text 4"]'
     );
     this.wait = parseInt(this.getAttribute("data-wait") ?? 2000, 10);
     this.forwardDelay = parseInt(this.getAttribute("data-forward") ?? 200, 10);
@@ -126,182 +127,3 @@ class TypeWriter extends HTMLElement {
 }
 
 window.customElements.define("type-writer", TypeWriter);
-
-// const template = document.createElement("template");
-// template.innerHTML = `
-//   <style>
-//     .txt {
-
-//     }
-
-//     .type-writer {
-//       border-right: 0.08em solid #666;
-//       animation: blink-caret 0.75s step-end infinite;
-//     }
-
-//     @keyframes blink-caret {
-//       from,
-//       to {
-//         border-color: transparent;
-//       }
-//       50% {
-//         border-color: #666;
-//       }
-//     }
-
-//   </style>
-//   <span class="type-writer"></span>
-// `;
-
-// class TypeWriter extends HTMLElement {
-//   constructor() {
-//     super();
-//     this.shadow = this.attachShadow({ mode: "open" });
-//     this.shadow.appendChild(template.content.cloneNode(true));
-//     this.txtContainer = this.shadow.querySelector(".type-writer");
-//     this.words = JSON.parse(this.getAttribute("data-words"));
-//     this.wait = parseInt(this.getAttribute("data-wait"), 10) || 2000;
-//     this.forwardDelay = parseInt(this.getAttribute("data-forward"), 10) || 200;
-//     this.backwardDelay =
-//       parseInt(this.getAttribute("data-backward"), 10) || 200;
-//     this.txt = "";
-//     this.wordIndex = 0;
-//     this.typeTimeout = null;
-//     this.isDeleting = false;
-//   }
-
-//   connectedCallback() {
-//     this.type();
-//   }
-
-//   disconnectedCallback() {
-//     clearTimeout(this.typeTimeout);
-//   }
-
-//   type() {
-//     const current = this.wordIndex % this.words.length;
-//     const fullTxt = this.words[current];
-
-//     if (this.isDeleting) {
-//       this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//       this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-
-//     this.txtContainer.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-//     let typeSpeed;
-//     if (this.isDeleting) {
-//       typeSpeed = this.backwardDelay;
-//     } else if (this.txt.length === fullTxt.length) {
-//       typeSpeed = this.wait;
-//       this.isDeleting = true;
-//     } else {
-//       typeSpeed = this.forwardDelay;
-//     }
-
-//     this.typeTimeout = setTimeout(() => this.type(), typeSpeed);
-
-//     // Start deleting the word after a brief pause once it's fully typed
-//     if (this.txt.length === fullTxt.length && !this.isDeleting) {
-//       setTimeout(() => {
-//         this.isDeleting = true;
-//       }, this.wait);
-//     }
-
-//     // Start typing the next word after deleting the current word
-//     if (this.txt.length === 0 && this.isDeleting) {
-//       this.wordIndex++;
-//       this.isDeleting = false;
-//       clearTimeout(this.typeTimeout);
-//       setTimeout(() => {
-//         this.type();
-//       }, this.forwardDelay);
-//     }
-//   }
-// }
-
-// window.customElements.define("type-writer", TypeWriter);
-
-// const template = document.createElement("template");
-// template.innerHTML = `
-//   <style>
-//     .txt {
-//       border-right: 0.08em solid #666;
-//     }
-//   </style>
-//   <span class="type-writer"></span>
-// `;
-
-// class TypeWriter extends HTMLElement {
-//   constructor() {
-//     super();
-//     this.shadow = this.attachShadow({ mode: "open" });
-//     this.shadow.appendChild(template.content.cloneNode(true));
-//     this.txtContainer = this.shadow.querySelector(".type-writer");
-//     this.words = JSON.parse(this.getAttribute("data-words"));
-//     this.wait = parseInt(this.getAttribute("data-wait"), 10) || 2000;
-//     this.forwardDelay = parseInt(this.getAttribute("data-forward"), 10) || 200;
-//     this.backwardDelay =
-//       parseInt(this.getAttribute("data-backward"), 10) || 200;
-//     this.txt = "";
-//     this.wordIndex = 0;
-//     this.typeTimeout = null;
-//     this.isDeleting = false;
-//     this.typos = parseInt(this.getAttribute("data-typos"), 10) || 0;
-//   }
-
-//   connectedCallback() {
-//     this.type();
-//   }
-
-//   disconnectedCallback() {
-//     clearTimeout(this.typeTimeout);
-//   }
-
-//   type() {
-//     const current = this.wordIndex % this.words.length;
-//     const fullTxt = this.words[current];
-
-//     if (this.isDeleting) {
-//       this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//       if (
-//         this.typos > 0 &&
-//         this.txt.length === Math.floor(fullTxt.length / 2)
-//       ) {
-//         // Vertippen
-//         let typoTxt = "";
-//         for (let i = 0; i < this.typos; i++) {
-//           typoTxt += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-//         }
-//         this.txt = fullTxt.substring(0, this.txt.length) + typoTxt;
-//       } else {
-//         this.txt = fullTxt.substring(0, this.txt.length + 1);
-//       }
-//     }
-
-//     this.txtContainer.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-//     let typeSpeed;
-//     if (this.isDeleting) {
-//       typeSpeed = this.backwardDelay;
-//     } else if (this.txt.length === fullTxt.length) {
-//       typeSpeed = this.wait;
-//       this.isDeleting = true;
-//     } else {
-//       typeSpeed = this.forwardDelay;
-//     }
-
-//     if (this.isDeleting && this.txt === "") {
-//       // Pause zwischen den Wörtern
-//       typeSpeed = 500;
-//       this.isDeleting = false;
-//       this.wordIndex++;
-//     }
-
-//     this.typeTimeout = setTimeout(() => this.type(), typeSpeed);
-//   }
-// }
-
-// window.customElements.define("type-writer", TypeWriter);
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ The TypeWriter web component is a simple and customizable typewriter effect elem
 To integrate the TypeWriter web component into your application, simply download the typewriter.js file and add it to your HTML document:
 
 ```html
-<script src="path/to/typewriter.js"></script>
+<script src="path/to/TypeWriter.js"></script>
 ```
 
 Alternatively, you can also include the file via a CDN connection:
@@ -25,7 +25,7 @@ To use the TypeWriter web component, simply add the type-writer tag to your HTML
 
 ```html
 <type-writer
-  data-words='["Frontend Developer", "UX / UI Designer", "Human", "Thinker", "Animal Lover", "Footballer"]'
+  data-words='["Text 1", "Text 2", "Text 3", "Text 4"]'
   data-forward="200"
   data-backward="100"
   data-wait="2000"
@@ -43,7 +43,7 @@ The TypeWriter web component supports the following attributes:
 - data-wait: The pause time in milliseconds before the typewriter effect restarts. By default, it's 2000 milliseconds.
 - data-forward: The delay time in milliseconds before a new letter is added. By default, it's 200 milliseconds.
 - data-backward: The delay time in milliseconds before a letter is deleted. By default, it's 100 milliseconds.
-  indicator: Specifies whether the blinking cursor should be displayed. By default, it's true.
+- indicator: Specifies whether the blinking cursor should be displayed. By default, it's true.
 - data-indicator-speed: The speed at which the blinking cursor blinks. By default, the speed is 0.75 seconds.
 
 ## Styling
@@ -52,10 +52,11 @@ The TypeWriter web component can be customized with CSS styles. You can use the 
 
 ```css
 .type-writer {
-  --color: #666;
-  --background: transparent;
+  --color: #000;
+  --background: none;
   --font-size: 1.5rem;
   --font-family: "Courier New", Courier, monospace;
+  --indicator-width: 5px;
 }
 ```
 
